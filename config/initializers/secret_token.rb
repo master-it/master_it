8e85d39750e63979561818ad3dd097d986141a8e
Plugin73 <laplugin73@gmail.com>
2013-01-27 15:31:18 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MasterIt::Application.config.secret_token = 'c76d9beb7f44b6d340606f076302d74cfb91e76bdb4ea3f814aca62f47ca6908eafb1be5a2e90dd030c4ee7341d4c4d50b1f6e125b7a01ff5124a01a2b1a68d2'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+MasterIt::Application.routes.draw do
+
+  root :to => 'welcome#index'
+
+end
